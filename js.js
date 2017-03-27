6db04a8c9cfa09d7ce03c86283fc19abf7062186
maggie jv <maggie jv>
2017-03-27 13:20:23 -0600

    lo basico funciona

--- js.js
+++ js.js
@@ -2,29 +2,19 @@ function mostrar(){
   var texto = document.getElementById("escribir").value;
   var copia =document.getElementById("preview");
       copia.innerHTML = texto;
-}
-function enviar(){
-  var contenedorCom= document.getElementById("contenedor");
-  var preview= document.getElementById("preview");
-  var nodo= preview.cloneNode(true);
-  nodo.id= new Date();
-  var ele =document.getElementById("escribir");
-     contenedorCom.appendChild(nodo);
-     ele.innerText="";
-     preview.innerHTML="";
-}
+ }
 function textoG(){
-var preview =document.getElementById("preview");
+  var preview =document.getElementById("preview");
     preview.style.fontSize = "48px";
  }
 function textoM(){
      var preview =document.getElementById("preview");
      preview.style.fontSize = "26px";
-  }
+}
 function textoP(){
      var preview =document.getElementById("preview");
       preview.style.fontSize = "15px";
-   }
+}
 
 function alinearI(){
     var preview =document.getElementById("preview");
@@ -47,4 +37,23 @@ function colorF(){
     var preview =document.getElementById("preview");
     var color= prompt("Elige un color de fondo");
         preview.style.background = color;
+     }
+     function enviar(){
+       var contenedorCom= document.getElementById("contenedor");
+       var preview= document.getElementById("preview");
+       var nodo= preview.cloneNode(true);
+       var ele =document.getElementById("escribir");
+       var nuvBot=document.createElement("button");
+          nodo.id=  Date.now();
+          nuvBot.value=nodo.id;
+          nuvBot.innerText= "borrar";
+          nuvBot.onclick = function(){
+            var elementoDel=document.getElementById(this.value);
+            contenedorCom.removeChild(this);
+            contenedorCom.removeChild(elementoDel);
+      }
+          ele.value="";
+          preview.innerHTML="";
+          contenedorCom.insertBefore(nodo,contenedorCom.firstChild);
+          contenedorCom.insertBefore(nuvBot,contenedorCom.firstChild);
      }
